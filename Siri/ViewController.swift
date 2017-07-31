cd7f0c2309547e9e34f7f7a8fa8c3076a98daba6
guoliting <guoliting@didichuxing.com>
2017-07-31 16:19:59 +0800

    在info.plist文件里添加了两个键值： NSMicrophoneUsageDescription -为获取麦克风语音输入授权的自定义消息。注意这个语音输入授权仅仅只会在用户点击microphone按钮时发生。 NSSpeechRecognitionUsageDescription – 语音识别授权的自定义信息

--- Siri/ViewController.swift
+++ Siri/ViewController.swift
@@ -7,17 +7,46 @@
 //
 
 import UIKit
+import Speech
 
-
-class ViewController: UIViewController {
-	
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
 	
 	@IBOutlet weak var textView: UITextView!
 	@IBOutlet weak var microphoneButton: UIButton!
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))  //1
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+        microphoneButton.isEnabled = false  //2
+
+        speechRecognizer?.delegate = self  //3
+        
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in  //4
+            
+            var isButtonEnabled = false
+            
+            switch authStatus {  //5
+                case .authorized:
+                    isButtonEnabled = true
+                    
+                case .denied:
+                    isButtonEnabled = false
+                    print("User denied access to speech recognition")
+                    
+                case .restricted:
+                    isButtonEnabled = false
+                    print("Speech recognition restricted on this device")
+                    
+                case .notDetermined:
+                    isButtonEnabled = false
+                    print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.microphoneButton.isEnabled = isButtonEnabled
+            }
+        }
 	}
 
 	@IBAction func microphoneTapped(_ sender: AnyObject) {
