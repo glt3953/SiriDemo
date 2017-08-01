a409f196eec94592f9f90467887d9f52f8c20d1b
guoliting <guoliting@didichuxing.com>
2017-08-01 10:44:30 +0800

    调整目录结构

---
+++ SiriDemoByObjectiveC/SiriDemoByObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SiriDemoByObjectiveC
+//
+//  Created by guoliting on 2017/8/1.
+//  Copyright © 2017年 NingXia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SiriDemoByObjectiveC/SiriDemoByObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SiriDemoByObjectiveC
+//
+//  Created by guoliting on 2017/8/1.
+//  Copyright © 2017年 NingXia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
